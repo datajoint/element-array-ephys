6c490f8a96a09d0e681c63b36951d4def1fcbd7b
Thinh Nguyen <thinh@vathes.com>
2021-03-23 11:29:10 -0500

    cleanup

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 long_description = """"
-DataJoint Elements for Extracellular Electrophysiology with Neuropixels probe
+DataJoint Element for Extracellular Electrophysiology with Neuropixels probe
 """
 
 with open(path.join(here, 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='elements-ephys',
+    name='element-array-ephys',
     version='0.0.1',
-    description="DataJoint Elements for Extracellular Electrophysiology ",
+    description="DataJoint Element for Extracellular Array Electrophysiology ",
     long_description=long_description,
     author='DataJoint NEURO',
     author_email='info@vathes.com',
     license='MIT',
-    url='https://github.com/datajoint/elements-ephys',
+    url='https://github.com/datajoint/element-array-ephys',
     keywords='neuroscience electrophysiology science datajoint',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     scripts=[],
