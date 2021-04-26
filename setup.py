1cbc62aaf9ba42534666e4790debdd3eba5a88d4
Thinh Nguyen <thinh@vathes.com>
2021-04-26 14:25:28 -0500

    re-work `pkg_name` and use README as `long_description`

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup, find_packages
 from os import path
 
-pkg_name = 'element_array_ephys'
+pkg_name = 'element-array-ephys'
 here = path.abspath(path.dirname(__file__))
 
-long_description = """"
-DataJoint Element for Extracellular Array Electrophysiology with Neuropixels probe
-"""
+with open(path.join(here, 'README.md'), 'r') as f:
+    long_description = f.read()
 
 with open(path.join(here, 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
-with open(path.join(here, pkg_name, 'version.py')) as f:
+with open(path.join(here, pkg_name.replace('-', '_'), 'version.py')) as f:
     exec(f.read())
 
 setup(
-    name='element-array-ephys',
+    name=pkg_name,
     version=__version__,
     description="DataJoint Element for Extracellular Array Electrophysiology",
     long_description=long_description,
