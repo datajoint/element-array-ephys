ef3578e3a6c1eaf3e082c0558c368b49cceb6a24
Thinh Nguyen <thinh@vathes.com>
2021-04-26 15:07:47 -0500

    Apply suggestions from code review
    Co-authored-by: Raphael Guzman <38401847+guzman-raphael@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os import path
 
-pkg_name = 'element-array-ephys'
+pkg_name = [p for p in find_packages() if '.' not in p][0]
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md'), 'r') as f:
@@ -10,11 +10,11 @@ with open(path.join(here, 'README.md'), 'r') as f:
 with open(path.join(here, 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
-with open(path.join(here, pkg_name.replace('-', '_'), 'version.py')) as f:
+with open(path.join(here, pkg_name, 'version.py')) as f:
     exec(f.read())
 
 setup(
-    name=pkg_name,
+    name=pkg_name.replace('_', '-'),
     version=__version__,
     description="DataJoint Element for Extracellular Array Electrophysiology",
     long_description=long_description,
