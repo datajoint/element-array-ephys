ba28637496fbea77207a81fae3e6a287c56b494a
Thinh Nguyen <thinh@vathes.com>
2021-10-01 11:39:58 -0500

    handles a weird windows/unix path incompatibility (even with pathlib)

--- element_array_ephys/__init__.py
+++ element_array_ephys/__init__.py
@@ -13,20 +13,20 @@ def find_full_path(root_directories, relative_path):
      from provided potential root directories (in the given order)
         :param root_directories: potential root directories
         :param relative_path: the relative path to find the valid root directory
-        :return: root_directory (pathlib.Path object)
+        :return: full-path (pathlib.Path object)
     """
-    relative_path = pathlib.Path(relative_path)
+    relative_path = _to_Path(relative_path)
 
     if relative_path.exists():
         return relative_path
 
     # turn to list if only a single root directory is provided
     if isinstance(root_directories, (str, pathlib.Path)):
-        root_directories = [root_directories]
+        root_directories = [_to_Path(root_directories)]
 
     for root_dir in root_directories:
-        if (pathlib.Path(root_dir) / relative_path).exists():
-            return pathlib.Path(root_dir) / relative_path
+        if (_to_Path(root_dir) / relative_path).exists():
+            return _to_Path(root_dir) / relative_path
 
     raise FileNotFoundError('No valid full-path found (from {})'
                             ' for {}'.format(root_directories, relative_path))
@@ -37,25 +37,33 @@ def find_root_directory(root_directories, full_path):
     Given multiple potential root directories and a full-path,
     search and return one directory that is the parent of the given path
         :param root_directories: potential root directories
-        :param full_path: the relative path to search the root directory
-        :return: full-path (pathlib.Path object)
+        :param full_path: the full path to search the root directory
+        :return: root_directory (pathlib.Path object)
     """
-    full_path = pathlib.Path(full_path)
+    full_path = _to_Path(full_path)
 
     if not full_path.exists():
         raise FileNotFoundError(f'{full_path} does not exist!')
 
     # turn to list if only a single root directory is provided
     if isinstance(root_directories, (str, pathlib.Path)):
-        root_directories = [root_directories]
+        root_directories = [_to_Path(root_directories)]
 
     try:
-        return next(pathlib.Path(root_dir) for root_dir in root_directories
-                    if pathlib.Path(root_dir) in set(full_path.parents))
+        return next(_to_Path(root_dir) for root_dir in root_directories
+                    if _to_Path(root_dir) in set(full_path.parents))
 
     except StopIteration:
         raise FileNotFoundError('No valid root directory found (from {})'
                                 ' for {}'.format(root_directories, full_path))
+
+
+def _to_Path(path):
+    """
+    Convert the input "path" into a pathlib.Path object
+    Handles one odd Windows/Linux incompatibility of the "\\"
+    """
+    return pathlib.Path(str(path).replace('\\', '/'))
 
 
 def dict_to_uuid(key):
