2ce12c1cfed6e4e983dd19cfcb5f0d021f49c845
Kabilar Gunalan <kabilar@datajoint.com>
2022-06-01 13:03:41 -0500

    Update length

--- element_array_ephys/ephys_precluster.py
+++ element_array_ephys/ephys_precluster.py
@@ -297,7 +297,7 @@ class PreClusterParamSteps(dj.Manual):
     # When pre-clustering is not performed, do not create an entry in `Step` Part table
     precluster_param_steps_id: smallint
     ---
-    precluster_param_steps_desc: varchar(256)
+    precluster_param_steps_desc: varchar(128)
     """
 
     class Step(dj.Part):
