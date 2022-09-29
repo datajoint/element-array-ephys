2d76efcccb4f47569a4de9f4c4a2ca215b45146c
Thinh Nguyen <thinh@vathes.com>
2022-09-29 16:30:11 -0500

    bugfix QC ingestion

--- element_array_ephys/ephys_no_curation.py
+++ element_array_ephys/ephys_no_curation.py
@@ -904,6 +904,7 @@ class QualityMetrics(dj.Imported):
 
         metrics_df = pd.read_csv(metric_fp)
         metrics_df.set_index('cluster_id', inplace=True)
+        metrics_df.replace([np.inf, -np.inf], np.nan, inplace=True)
 
         metrics_list = [
             dict(metrics_df.loc[unit_key['unit']], **unit_key)
