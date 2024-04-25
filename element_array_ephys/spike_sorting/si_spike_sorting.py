917d5cad1add9359c69367d04e2cce05b6dfed48
MilagrosMarin <milmarina02@gmail.com>
2024-04-25 22:14:00 +0200

    Resolved ValueError in Electrode Selection Logic

--- element_array_ephys/spike_sorting/si_spike_sorting.py
+++ element_array_ephys/spike_sorting/si_spike_sorting.py
@@ -114,8 +114,10 @@ class PreProcessing(dj.Imported):
 
         # Filter for used electrodes. If probe_info["used_electrodes"] is None, it means all electrodes were used.
         number_of_electrodes = len(electrode_query)
-        probe_info["used_electrodes"] = probe_info["used_electrodes"] or list(
-            range(number_of_electrodes)
+        probe_info["used_electrodes"] = (
+            probe_info["used_electrodes"]
+            if probe_info["used_electrodes"] is not None
+            else list(range(number_of_electrodes))
         )
         unused_electrodes = [
             elec
